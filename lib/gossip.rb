4114c19bf8e843b28f2e48c068f5913266b0404f
Arthur Vu <arthurvu75@gmail.com>
2019-10-21 15:52:14 +0200

    Update gossip.rb

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,18 +1,11 @@
 require 'csv'
 
 class Gossip
-  attr_accessor :author, :content
-
-  def initialize(author, content)
-      @author = author
-      @content = content
-  end
-
-  def save
-      CSV.open("./db/gossip.csv", "ab") do |csv|
-        csv << ["Mon super auteur", "Ma super description"]
-  end
-  end
+    def save
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+          csv << ["Mon super auteur", "Ma super description"]
+        end
+    end
 end
 
 
